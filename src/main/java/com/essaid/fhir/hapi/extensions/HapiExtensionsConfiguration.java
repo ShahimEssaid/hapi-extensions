e049dcb261b0b783b4066c60aacbc137e7850b41
Shahim Essaid <shahim@essaid.com>
2023-01-09 14:21:17 -0800

    A static web configuration for /static/**

--- src/main/java/com/essaid/fhir/hapi/extensions/HapiExtensionsConfiguration.java
+++ src/main/java/com/essaid/fhir/hapi/extensions/HapiExtensionsConfiguration.java
@@ -1,14 +1,16 @@
 package com.essaid.fhir.hapi.extensions;
 
 import com.essaid.fhir.hapi.extensions.provider.HelloHapi;
+import com.essaid.fhir.hapi.extensions.web.StaticWebConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({HapiExtensionsProperties.class, RestfulServerPostProcessor.class})
+@Import({HapiExtensionsProperties.class, RestfulServerPostProcessor.class, StaticWebConfiguration.class})
 @ComponentScan(basePackageClasses = HelloHapi.class)
+
 public class HapiExtensionsConfiguration {
 
-  
+
 }
