4b14d5c8ef851ac5b961a658c4146f4fc3377f18
Shahim Essaid <shahim@essaid.com>
2023-03-27 10:14:39 -0700

    Refactoring provider configuration and moving out concept search example.

--- src/main/java/com/essaid/fhir/hapi/ext/HapiExtensionsProperties.java
+++ src/main/java/com/essaid/fhir/hapi/ext/HapiExtensionsProperties.java
@@ -4,20 +4,48 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ConfigurationProperties(prefix = "com.essaid.fhir.hapi.extensions")
 @Configuration
 public class HapiExtensionsProperties {
 
-    private List<String> providerClassNames = new ArrayList<>();
+//    private List<String> providerClassNames = new ArrayList<>();
 
-    public List<String> getProviderClassNames() {
-        return providerClassNames;
+    public Map<String, Provider> getProviders() {
+        return providers;
     }
 
-    public void setProviderClassNames(List<String> providerClassNames) {
-        this.providerClassNames = providerClassNames;
-    }
+    private final Map<String, Provider> providers = new HashMap<>();
 
+//    public List<String> getProviderClassNames() {
+//        return providerClassNames;
+//    }
+//
+//    public void setProviderClassNames(List<String> providerClassNames) {
+//        this.providerClassNames = providerClassNames;
+//    }
+//
+
+
+    public static class Provider {
+        public String getClassName() {
+            return className;
+        }
+
+        public void setClassName(String className) {
+            this.className = className;
+        }
+
+        private String className;
+
+        @Override
+        public String toString() {
+            return "Provider{" +
+                    "className='" + className + '\'' +
+                    '}';
+        }
+    }
 }
--- src/main/java/com/essaid/fhir/hapi/ext/processor/RestfulServerPostProcessor.java
+++ src/main/java/com/essaid/fhir/hapi/ext/processor/RestfulServerPostProcessor.java
@@ -14,46 +14,61 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestfulServerPostProcessor implements BeanPostProcessor, ApplicationContextAware {
 
-    private final Environment environment;
-    HapiExtensionsProperties properties;
-    private ApplicationContext context;
+  private final Environment environment;
+  HapiExtensionsProperties properties;
+  private ApplicationContext context;
 
-    public RestfulServerPostProcessor(HapiExtensionsProperties properties, Environment environment) {
-        this.properties = properties;
-        this.environment = environment;
-    }
+  public RestfulServerPostProcessor(HapiExtensionsProperties properties, Environment environment) {
+    this.properties = properties;
+    this.environment = environment;
+  }
 
-    @Override
-    public Object postProcessAfterInitialization(@NotNull Object bean, String beanName) throws BeansException {
+  @Override
+  public Object postProcessAfterInitialization(@NotNull Object bean, String beanName) throws BeansException {
 
-        if (beanName.equals("restfulServer")) {
-            RestfulServer hapiServer = (RestfulServer) bean;
-            properties.getProviderClassNames().forEach(s -> {
-                try {
-                    hapiServer.registerProvider(this.context.getBean(RestfulServerPostProcessor.class.getClassLoader().loadClass(s)));
-                    System.out.println("============= Extensions provider: " + s);
-
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-
-                if (environment.acceptsProfiles(activeProfiles -> {
-                    return activeProfiles.test("x");
-                })) {
-                  hapiServer.registerProvider(this.context.getBean(XPrintIndexMappingProvider.class));
-                }
-
-            });
+    if (beanName.equals("restfulServer")) {
+      RestfulServer hapiServer = (RestfulServer) bean;
+      properties.getProviders().forEach((providerKey, providerConfig) -> {
+        try {
+          Object provider = this.context.getBean(RestfulServerPostProcessor.class.getClassLoader().loadClass(providerConfig.getClassName()));
+          if (provider != null) {
+            System.out.println("=============== Loading provider:: " + providerConfig);
+            hapiServer.registerProvider(provider);
+          } else {
+            System.out.println("=============== Provider not found: " + providerConfig);
+          }
 
 
-            System.out.println("============= Extensions providers done");
+        } catch (ClassNotFoundException e) {
+          throw new RuntimeException(e);
         }
+      });
+//            properties.getProviderClassNames().forEach(s -> {
+//                try {
+//                    hapiServer.registerProvider(this.context.getBean(RestfulServerPostProcessor.class.getClassLoader().loadClass(s)));
+//                    System.out.println("============= Extensions provider: " + s);
+//
+//                } catch (ClassNotFoundException e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//                if (environment.acceptsProfiles(activeProfiles -> {
+//                    return activeProfiles.test("x");
+//                })) {
+//                  hapiServer.registerProvider(this.context.getBean(XPrintIndexMappingProvider.class));
+//                }
+//
+//            });
 
-        return bean;
+
+      System.out.println("============= Extensions providers done");
     }
 
-    @Override
-    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
-    }
+    return bean;
+  }
+
+  @Override
+  public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+    this.context = applicationContext;
+  }
 }
